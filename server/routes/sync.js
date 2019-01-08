7d2e4c49e317d024d43e908fd43070f7496e1300
Darren Segal <me@darren-segal.com>
2019-01-08 17:41:22 -0500

    Added collection sync

--- server/routes/sync.js
+++ server/routes/sync.js
@@ -53,56 +53,59 @@ router.get('/', (req, res) => {
         let collections = [].concat(...collection_sets.map(col => {
             return col.data.smart_collections || col.data.custom_collections
         }))
-
-        // res.status(200).send(collections)
-
-        // console.log(collections)
-        let requests = []
-
-        collections.forEach(collection => {
-            http.get('products/count.json', {
+        
+        collections = collections.map(collection => {
+            return http.get('products/count.json', {
                 params: { collection_id: collection.id }
             }).then(collection_count_res => {
-
+                let requests = []
                 let pages = Math.ceil(collection_count_res.data.count / 250)
-                for (let i = 1; i <= pages; i++) {
-                    let request = http.get('products.json', {
-                        params: {
-                            collection_id: collection.id,
-                            limit: 250,
-                            page: i
-                        }
-                    }).catch(err => console.log(err))
-                    requests.push(request)
+
+                if (pages > 0) {
+                    for (let i = 1; i <= pages; i++) {
+                        let request = http.get('products.json', {
+                            params: {
+                                collection_id: collection.id,
+                                limit: 250,
+                                page: i
+                            }
+                        }).catch(err => console.log(err))
+                        requests.push(request)
+                    }
+                    
+                    return Promise.all(requests).then(responses => {
+                        let products = [].concat(...responses.map(item => {
+                            return [].concat(...item.data.products.map(item => [
+                                { index: { _index: `collection_${collection.handle}`, _type: 'product', _id: item.id } },
+                                item
+                            ]))
+                        }))
+
+                        return products
+                        
+                    }).catch(err => console.error(err))
+                } else {
+                    return []
                 }
-
-                console.log(requests)
-                Promise.all(requests).then(responses => {
-                    let products = [].concat(...responses.map(item => {
-                        return [].concat(...item.data.products.map(item => [
-                            { index: { _index: `collections/${collection.handle}`, _type: 'product', _id: item.id } },
-                            item
-                        ]))
-                    }))
-    
-                    console.log(products)
-    
-                    res.status(200).send(products)
-    
-                    // es_client.bulk({
-                    //     body: products
-                    // }, (err, index_response) => {
-                    //     if (err) {
-                    //         res.status(500).send(err)
-                    //     } else {
-                    //         res.status(200).send(index_response)
-                    //     }
-                    // })
-    
-                }).catch(err => console.error(err))
             })
-
+            
         })
+        
+        Promise.all(collections).then(values => {
+            // console.log(values)
+            let products = [].concat(...values.filter(item => item.length))
+            es_client.bulk({
+                body: products
+            }, (err, index_response) => {
+                if (err) {
+                    console.log(err)
+                } else {
+                    console.log(index_response)
+                    res.status(200).send(index_response)
+                }
+            })
+        })
+
     }).catch(err => console.error(err))
 })
 
